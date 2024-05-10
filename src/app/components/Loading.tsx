38a73f123d136ed649d0e9e09aff297387e6ef2d
Zerihun Moges <zerihunmoges78@gmail.com>
2024-05-11 00:02:02 +0300

    fix build error

---
+++ src/app/components/Loading.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Loading = () => {
+  return (
+    <div className="w-8 h-8 border-2 border-dashed rounded-full animate-spin"></div>
+  );
+};
+
+export default Loading;
--- src/app/globe/Earth.tsx
+++ src/app/globe/Earth.tsx
@@ -8,9 +8,11 @@ import React, {
 } from "react";
 import Globe from "react-globe.gl";
 import TimelineContext from "./timeline/TimelineContext";
+import Loading from "../components/Loading";
 
 const Earth = () => {
   const context = useContext(TimelineContext);
+  const globeEl = useRef();
 
   if (!context) {
     throw new Error("Timeline must be used within a TimelineProvider");
@@ -23,12 +25,12 @@ const Earth = () => {
 
   useEffect(() => {
     async function getMap() {
-      try {
-        const year =
-          Number(selectedYear.split(" ")[0]) *
-          (selectedYear.includes("BC") ? -1 : 1);
+      startYearTransition(async () => {
+        try {
+          const year =
+            Number(selectedYear.split(" ")[0]) *
+            (selectedYear.includes("BC") ? -1 : 1);
 
-        startYearTransition(async () => {
           const response = await fetch(
             `https://histori.onrender.com/api/map?period=${year}`
           );
@@ -37,49 +39,56 @@ const Earth = () => {
           }
           const data = await response.json(); // Parse response body as JSON
           setData(data.features);
-        });
-      } catch (error) {
-        console.error("Error fetching GeoJSON data:", error);
-      }
+        } catch (error) {
+          console.error("Error fetching GeoJSON data:", error);
+        }
+      });
     }
     getMap();
   }, [selectedYear]);
 
   return (
-    <div style={{ position: "relative", width: "100%", height: "100vh" }}>
-      <div className="absolute top-5 left-5 bg-green-500 p-8 z-50">
+    <div
+      className="relative"
+      style={{ position: "relative", width: "100%", height: "100vh" }}
+    >
+      <div className="absolute bottom-16 right-8 z-50">
         {" "}
-        {isYearTransitionPending ? "Loading..." : "complete"}
+        {isYearTransitionPending && <Loading />}
       </div>
-      <Globe
-        polygonsData={data}
-        animateIn={true}
-        polygonAltitude={({ properties: d }: any) =>
-          d?.NAME === selectedCountry && d?.NAME !== null ? 0.01 : 0.0015
-        }
-        polygonStrokeColor={({ properties: d }: any) =>
-          d?.NAME === selectedCountry && d?.NAME !== null
-            ? "rgba(0, 50, 255, 0.7)"
-            : "rgba(255, 255, 255, 0.2)"
-        }
-        polygonCapColor={({ properties: d }: any) =>
-          d?.NAME === selectedCountry && d?.NAME !== null
-            ? "rgba(0, 50, 255, 0.3)"
-            : "rgba(255, 255, 255, 0)"
-        }
-        polygonSideColor={({ properties: d }: any) => "rgba(50, 0, 255, 0)"}
-        polygonLabel={({ properties: d }: any) =>
-          d?.NAME !== null ? `<b>${d?.NAME} </b> <br />` : ""
-        }
-        onPolygonClick={(polygon: any) => {
-          setCountry(polygon?.properties.NAME);
-        }}
-        onPolygonHover={(polygon: any, prevPolygon) => {
-          setSelectedCountry(polygon ? polygon.properties.NAME : "");
-        }}
-        globeImageUrl="/earthmap.jpeg"
-        bumpImageUrl="/earthbump.jpeg"
-      />
+
+      {typeof window !== "undefined" && (
+        <Globe
+          ref={globeEl}
+          polygonsData={data}
+          animateIn={true}
+          polygonAltitude={({ properties: d }: any) =>
+            d?.NAME === selectedCountry && d?.NAME !== null ? 0.01 : 0.0015
+          }
+          polygonStrokeColor={({ properties: d }: any) =>
+            d?.NAME === selectedCountry && d?.NAME !== null
+              ? "rgba(0, 50, 255, 0.7)"
+              : "rgba(255, 255, 255, 0.2)"
+          }
+          polygonCapColor={({ properties: d }: any) =>
+            d?.NAME === selectedCountry && d?.NAME !== null
+              ? "rgba(0, 50, 255, 0.3)"
+              : "rgba(255, 255, 255, 0)"
+          }
+          polygonSideColor={({ properties: d }: any) => "rgba(50, 0, 255, 0)"}
+          polygonLabel={({ properties: d }: any) =>
+            d?.NAME !== null ? `<b>${d?.NAME} </b> <br />` : ""
+          }
+          onPolygonClick={(polygon: any) => {
+            setCountry(polygon?.properties.NAME);
+          }}
+          onPolygonHover={(polygon: any, prevPolygon) => {
+            setSelectedCountry(polygon ? polygon.properties.NAME : "");
+          }}
+          globeImageUrl="/earthmap.jpeg"
+          bumpImageUrl="/earthbump.jpeg"
+        />
+      )}
     </div>
   );
 };
