0d05280e70d8ae01d6a8af57da6e67f12226d277
Yared04 <yaredt29@gmail.com>
2024-06-14 11:18:48 +0300

    fix: change no results illustration

--- src/app/components/LandingLayout.tsx
+++ src/app/components/LandingLayout.tsx
@@ -21,35 +21,37 @@ const Landing = ({
   }, []);
 
   return (
-    <div className="bg-[url('/Earth0230.png')] h-screen w-screen bg-contain bg-no-repeat bg-center py-10 px-16 flex justify-between">
-      <div className=" flex flex-col justify-around">
-        <div className=" flex justify-between basis1/3">
-          <h1
-            onClick={() => {
-              router.push("/");
-            }}
-            className="text-white cursor-pointer font-semibold text-6xl"
+    <div className="bg-[url('/stars.png')]">
+      <div className="bg-[url('/Earth0230.png')] h-screen w-screen bg-contain bg-no-repeat bg-center py-10 px-16 flex justify-between">
+        <div className=" flex flex-col justify-around">
+          <div className=" flex justify-between basis1/3">
+            <h1
+              onClick={() => {
+                router.push("/");
+              }}
+              className="text-white cursor-pointer font-semibold text-6xl"
+            >
+              HISTOR<span className="text-blue-600">I</span>
+            </h1>
+          </div>
+          <div className="basis-2/3 content-center">
+            <h3 className="text-white font-normal text-6xl leading-normal">
+              Discover <br /> the world as <br /> it was
+            </h3>
+          </div>
+        </div>
+
+        <div className="flex flex-col justify-between">
+          <button
+            onClick={handleClick}
+            className="text-white font-normal text-xl leading-normal text-end"
           >
-            HISTOR<span className="text-blue-600">I</span>
-          </h1>
-        </div>
-        <div className="basis-2/3 content-center">
-          <h3 className="text-white font-normal text-6xl leading-normal">
-            Discover <br /> the world as <br /> it was
-          </h3>
-        </div>
-      </div>
+            {showLogin ? "Login" : ""}
+          </button>
 
-      <div className="flex flex-col justify-between">
-        <button
-          onClick={handleClick}
-          className="text-white font-normal text-xl leading-normal text-end"
-        >
-          {showLogin ? "Login" : ""}
-        </button>
-
-        {children}
-        <div></div>
+          {children}
+          <div></div>
+        </div>
       </div>
     </div>
   );
